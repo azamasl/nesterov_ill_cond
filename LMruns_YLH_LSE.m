709342c11ecc2bd6553c85f2178637632cff1057
azamasl <aa2821@nyu.edu>
2020-02-10 17:43:44 -0500

    initial commit

--- LMruns_YLH_LSE.m
+++ LMruns_YLH_LSE.m
@@ -4,34 +4,24 @@ addpath('/Users/azam/google/projects/mCode/mlibs/hanso2_2')
 muvalues = 10.^(-16:0) % this is OK for n=10, for bigger n, takes too long
 for k=1:length(muvalues) % 2018: using only LBFGS-1
     mu = muvalues(k) 
-    % explicit smoothing, set options as well as pars
 
     [pars,options] = pars_YLH_LSE(n,startpert,inf,mu);
     options.maxit = maxit;
     options.nvec = 0;
     options.scale = 1;
-    %options
     [x,f,d,H,iter,info] = bfgs(pars,options);       
-    %iter
-    %info
     fB(k) = f;
     dnormB(k) = norm(d);
     iterB(k) = iter;    
-    options.nvec = 1; % limited memory option: LBFGS-1 (2018, in conjunction with Azam's paper)
-    options.scale = 1; % actually this is the default
-    %options
+    options.nvec = 1; % limited memory option: LBFGS-1
+    options.scale = 1; 
     [x,f,d,H,iter,info] = bfgs(pars,options);    
-    %iter
-    %info
     fLB1scale(k) = f;
     dnormLB1scale(k) = norm(d);
     iterLB1scale(k) = iter;
     options.nvec = 1;
-    options.scale = 0; %%%%% turns out to be very bad choice???    
-    %options
+    options.scale = 0;    
     [x,f,d,H,iter,info] = bfgs(pars,options);    
-    %iter 
-    %info
     fLB1noscale(k) = f;
     dnormLB1noscale(k) = norm(d);
     iterLB1noscale(k) = iter;
